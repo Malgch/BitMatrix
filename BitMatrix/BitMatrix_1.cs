41b2574152469c3c0db8f65e0208b2b0ce32c7be
Malgch <malgorzata.chudy@microsoft.wsei.edu.pl>
2023-04-16 11:11:15 +0200

    .

--- BitMatrix/BitMatrix_1.cs
+++ BitMatrix/BitMatrix_1.cs
@@ -9,4 +9,27 @@ using System.Threading.Tasks;
 public partial class BitMatrix
 {
     //ToDo
+
+    public override string BitMatrix.ToString()
+    {
+        int x = NumberOfRows;
+        int y = NumberOfColumns;
+
+
+        for (int i = 0; i < x * y; i++ )
+        {
+            int rowLength = i;
+            if (rowLength % y != 0)
+            {
+                Console.WriteLine(data);
+            }
+            else
+            {
+                Console.WriteLine($"{data}{Environment.NewLine}");
+            }
+            
+        }
+      
+
+    }
 }
