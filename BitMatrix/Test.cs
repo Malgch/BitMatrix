4c3e35bbe2bdde19816cedc66cb4912ff8211d3e
Malgch <malgorzata.chudy@microsoft.wsei.edu.pl>
2023-04-16 10:37:52 +0200

    Dodaj pliki projektów.

---
+++ BitMatrix/Test.cs
@@ -0,0 +1,10 @@
+﻿
+
+// macierz bitów 4x3 wypełniona
+// domyślną wartością
+var m = new BitMatrix(4, 3);
+Console.WriteLine(m.ToString());
+
+// macierz bitów 3x4 wypełniona 1
+m = new BitMatrix(3, 4, 1);
+Console.WriteLine(m);
